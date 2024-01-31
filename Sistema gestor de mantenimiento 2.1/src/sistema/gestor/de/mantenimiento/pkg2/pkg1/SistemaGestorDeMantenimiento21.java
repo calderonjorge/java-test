8880676e70cebec293a820deb769ceacdaa583d3
renen <renen@LAPTOP-GJUJD7SO>
2024-01-30 19:02:05 -0600

    Primer version

---
+++ Sistema gestor de mantenimiento 2.1/src/sistema/gestor/de/mantenimiento/pkg2/pkg1/SistemaGestorDeMantenimiento21.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.gestor.de.mantenimiento.pkg2.pkg1;
+
+/**
+ *
+ * @author renen
+ */
+public class SistemaGestorDeMantenimiento21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
